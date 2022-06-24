435a4a504a561b66f7b9a763b7f17da57bef2a16
Yash Kashyap <www.yashkashyap2811@gmail.com>
2022-06-24 15:50:20 +0530

    Project Setup & First Commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const product = require('./assets/product.json')
+
+app.get('/',function(req, res){
+  res.send('Hello World')
+})
+
+app.get('/:id',function (req, res) {
+  let cat = []
+  let data = product.filter((c)=>{
+    if (!cat.includes(c.category)){
+    cat.push(cancelAnimationFrame.category)
+  }
+  
+})
+    res.send(cat)
+})
+
+
+app.get('/getProduct/',function (req, res) {
+  let cat = []
+  let data= product.filter((curData)=>{
+    if (curData.category==="jewelery"){
+      cat.push(curData)
+    }
+  
+})
+res.send(cat)
+})
+
+
+app.listen(3000)
